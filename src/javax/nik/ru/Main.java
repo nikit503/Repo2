59358500cb5bbd280b91f1c8655533d4b8548a87
NikitPC <nikit-antono2011@yandex.ru>
2018-02-10 22:25:29 +0300

    создаем xml
    Signed-off-by: NikitPC <nikit-antono2011@yandex.ru>

--- src/javax/nik/ru/Main.java
+++ src/javax/nik/ru/Main.java
@@ -1,10 +1,39 @@
 package javax.nik.ru;
 
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+
+
+import java.io.FileNotFoundException;
+
+
 import static javax.nik.ru.Utils.*;
 
 public class Main {
-    public static void main(String[] args) {
 
-        printTest("TestMsg");
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException, FileNotFoundException {
+
+        DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder=factory.newDocumentBuilder();
+        Document document=builder.newDocument();
+
+        Element root=document.createElement("root");
+        Element font=document.createElement("font");
+        Text text=document.createTextNode("Text example!");
+
+        document.appendChild(root);
+        root.appendChild(font);
+        font.appendChild(text);
+        font.setAttribute("size","20");
+
+        writeToFile(document,"output.xml");
     }
 }
--- src/javax/nik/ru/Utils.java
+++ src/javax/nik/ru/Utils.java
@@ -1,9 +1,25 @@
 package javax.nik.ru;
 
+import org.w3c.dom.Document;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 class Utils {
 
     static void printTest(String str){
         System.out.println(str);
     }
 
+    static void writeToFile(Document document, String fileName) throws FileNotFoundException, TransformerException {
+        Transformer transformer= TransformerFactory.newInstance().newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT,"yes");
+        transformer.transform(new DOMSource(document),new StreamResult(new FileOutputStream(fileName)));
+    }
 }
