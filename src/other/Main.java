ff7af5ccd393c03d8744cab02444ec43f0036ed8
NikitPC <nikit-antono2011@yandex.ru>
2018-07-07 16:22:23 +0300

    push
    Signed-off-by: NikitPC <nikit-antono2011@yandex.ru>

---
+++ src/other/Main.java
@@ -0,0 +1,15 @@
+package other;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String a = "12фцвawdawdawwd";
+        String b = "12фцвawdawdawwd";
+
+        String c =a;
+
+
+        System.out.println(a==c);
+    }
+}
